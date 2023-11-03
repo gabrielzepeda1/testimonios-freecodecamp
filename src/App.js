3dcab22a599f943b3c09480931877ef4c28802b5
Gabriel Zepeda <gabrielzepeda846@gmail.com>
2023-11-02 18:53:25 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Testimonio  from './componentes/Testimonio.js'
+import {Shawn, Sarah, Emma } from './js/testimonio-object.jsx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+      </div>
+      <Testimonio
+        nombre={Shawn.nombre}
+        pais={Shawn.pais}
+        imagen={Shawn.imagen}
+        alt={Shawn.alt}
+        cargo={Shawn.cargo}
+        empresa={Shawn.empresa}
+        testimonio={Shawn.testimonio}
+      />
+      <Testimonio
+        nombre={Sarah.nombre}
+        pais={Sarah.pais}
+        imagen={Sarah.imagen}
+        alt={Sarah.alt}
+        cargo={Sarah.cargo}
+        empresa={Sarah.empresa}
+        testimonio={Sarah.testimonio}
+      />
+      <Testimonio
+        nombre={Emma.nombre}
+        pais={Emma.pais}
+        imagen={Emma.imagen}
+        alt={Emma.alt}
+        cargo={Emma.cargo}
+        empresa={Emma.empresa}
+        testimonio={Emma.testimonio}
+      />
     </div>
   );
 }
---
+++ src/js/testimonio-object.jsx
@@ -0,0 +1,45 @@
+// import Testimonio from "../componentes/Testimonio";
+
+function objTestimonio(nombre, pais, imagen, alt, cargo, empresa, testimonio) { 
+    this.nombre = nombre;
+    this.pais = pais;
+    this.imagen = imagen;
+    this.alt = alt;
+    this.cargo = cargo;
+    this.empresa = empresa;
+    this.testimonio = testimonio;
+}
+
+const Shawn = new objTestimonio
+(
+    'Shawn Wang', 
+    'Singapur', 
+    'shawn', 
+    'Imagen de Shawn',
+    'Ingeniero de Software',
+    'Amazon', 
+    'Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
+)
+
+const Sarah = new objTestimonio(
+    'Sarah Chima',
+    'Nigeria',
+    'sarah',
+    'Imagen de Sarah',
+    'Ingeniera de Software',
+    'ChatDesk',
+    'freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
+)
+
+const Emma = new objTestimonio
+(
+    'Emma Bostian',
+    'Suecia',
+    'emma',
+    'Imagen de Emma',
+    'Ingeniera de Software',
+    'Spotify',
+    'Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+)
+
+export {Shawn, Sarah, Emma}
